99272b14ba064b6b1873acfed12bf8b66b066553
joykeeper <av.citizen@gmail.com>
2018-09-23 22:16:52 -0400

    Here we go, the first commit with package.json file

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as Redux from 'redux';
+// import { createStore, combineReducers } as Redux from 'redux';
+
+
+
+
+console.log('asdf');
+console.log(React);
