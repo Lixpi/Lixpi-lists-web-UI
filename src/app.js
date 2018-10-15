1db78d79c7d248eec96bd6c37e86a590c07cb198
Anton <joykeeper@Antons-iMac.local>
2018-10-15 09:59:04 -0400

    learning curve 6

--- src/app.js
+++ src/app.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import * as Redux from 'redux';
 import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 
-import { addDeck, showAddDeck, hideAddDeck } from './actions';
 import * as reducers from './reducers';
 
 import App from './components/App';
@@ -18,15 +17,11 @@ const store = createStore(combineReducers(reducers));
 function run() {
     let state = store.getState();
 
-    ReactDOM.render(<App>
-        <Sidebar 
-            decks={state.decks} 
-            addingDeck={state.addingDeck} 
-            addDeck={name => store.dispatch(addDeck(name))}
-            showAddDeck={() => store.dispatch(showAddDeck())}
-            hideAddDeck={() => store.dispatch(hideAddDeck())}
-        />
-    </App>, document.getElementById('root'));
+    ReactDOM.render((<Provider store={store}>
+        <App>
+            <Sidebar />
+        </App>
+        </Provider>), document.getElementById('root'));
 }
 
 run();
