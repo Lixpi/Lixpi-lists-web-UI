35f467c9f908abb5f8c5bd71683820ca0aab21fa
Shelby Carter <lixpi@lixpi.com>
2018-11-12 22:58:08 -0500

    added bootstrap 4 grid, refactoring login page to utilize new grid is in progress

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -35,19 +35,6 @@ class LoginForm extends React.Component {
                  </form>
               </div>
             </div>
-            // <div id="login-page-container">
-            //     <div id="logo-container">
-            //         <img src="images/logo.png" alt="Lixpi Lists" />
-            //     </div>
-            //     <div id="login-form-container">
-            //         <form className="store-selector" onSubmit={this.goToStore}>
-            //             <h2>Who are you?</h2>
-            //             <input type="text" required placeholder="Username" ref={this.loginName} />
-            //             <input type="text" required placeholder="Password" ref={this.loginPassword} />
-            //             <button type="submit">Go</button>
-            //         </form>
-            //     </div>
-            // </div>
         )
     }
 }
