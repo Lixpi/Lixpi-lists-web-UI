133520b667952ac5ab94d1bf93ae8ca392b8f2a0
Anton Iskandiarov <av.citizen@gmail.com>
2019-06-02 20:57:08 -0400

    messing with react-select

--- src/components/TasksListing.js
+++ src/components/TasksListing.js
@@ -4,255 +4,28 @@ import { Calendar } from 'react-date-range';
 import Select from 'react-select';
 
 import TopNavBar from './TopNavBar';
-
-
-//Select
-const options = [
-  { value: 'chocolate', label: 'Chocolate' },
-  { value: 'strawberry', label: 'Strawberry' },
-  { value: 'vanilla', label: 'Vanilla' }
-];
-
-
+import TaskRow from './TaskRow';
+import NewTaskRow from './NewTaskRow';
 
 class TasksListing extends React.Component {
-
-    state = {
-        dates: null,
-        selectedOption: null,
-    }
-
-    onSelect = dates => this.setState({dates})
-
-
-    handleChange = (selectedOption) => {
-        this.setState({ selectedOption });
-        console.log(`Option selected:`, selectedOption);
-    }
-
-
     render() {
-        const { selectedOption } = this.state;
 
         return (
             <CSSTransition timeout={11000} classNames="post">
-
-
                 <div className="container full-height" id="login-page-container">
-                {/*<Calendar
-                    date={new Date()}
-                    onChange={this.handleSelect}
-                />*/}
-
                     <TopNavBar />
-
-
                     <div className="taksk-listing mt-4">
 
-                        <div className="color-coded-row code-red task-row">
-                            <div className="row no-gutters align-items-center">
-                                <div className="col-md-auto task-status">
-                                    <span className="icon-info-circled"></span>
-                                </div>
-                                <div className="col-md-auto">
-                                    <span className="icon-hash"></span>
-                                </div>
-                                <div className="col-md-auto">
-                                    <span className="task-key">ICC-215</span>
-                                </div>
-                                <div className="col-md-auto">
-                                    <span className="vertical-divider"></span>
-                                </div>
-                                <div className="col-md-auto">
-                                    <span className="task-title">Blocker task, requires immediate attention</span>
-                                </div>
-                                <div className="col">
-                                    <div className="row no-gutters align-items-center d-flex justify-content-end">
-                                        <div className="col-md-auto datepicker-container">
-                                            <span className="task-due">Apr/01/19</span>
-                                            <Calendar
-                date={new Date()}
-                onChange={this.handleSelect} />
-                                        </div>
-                                        <div className="col-md-auto task-assignee-avatar">
-                                            <img className="avatar" src="/images/avatar.jpg" alt="Kate" />
-                                        </div>
-                                        <div className="col-md-auto">
-                                            {/*<span className="task-assignee">Kate Wilson</span>*/}
-                                            <Select
-                                                value={selectedOption}
-                                                onChange={this.handleChange}
-                                                options={options}
-                                            />
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-
-                        <div className="color-coded-row code-yellow task-row">
-                            <div className="row no-gutters align-items-center">
-                                <div className="col-md-auto task-status">
-                                    <span className="icon-attention"></span>
-                                </div>
-                                <div className="col-md-auto">
-                                    <span className="icon-hash"></span>
-                                </div>
-                                <div className="col-md-auto">
-                                    <span className="task-key">ICC-215</span>
-                                </div>
-                                <div className="col-md-auto">
-                                    <span className="vertical-divider"></span>
-                                </div>
-                                <div className="col-md-auto">
-                                    <span className="task-title">Important task that requires special attention</span>
-                                </div>
-                                <div className="col">
-                                    <div className="row no-gutters align-items-center d-flex justify-content-end">
-                                        <div className="col-md-auto">
-                                            <span className="task-due">Apr/01/19</span>
-                                        </div>
-                                        <div className="col-md-auto task-assignee-avatar">
-                                            <img className="avatar" src="/images/avatar.jpg" alt="Kate" />
-                                        </div>
-                                        <div className="col-md-auto">
-                                            <span className="task-assignee">Kate Wilson</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="color-coded-row code-green task-row">
-                            <div className="row no-gutters align-items-center">
-                                <div className="col-md-auto task-status">
-                                    <span className="icon-info-circled"></span>
-                                </div>
-                                <div className="col-md-auto">
-                                    <span className="icon-hash"></span>
-                                </div>
-                                <div className="col-md-auto">
-                                    <span className="task-key">ICC-215</span>
-                                </div>
-                                <div className="col-md-auto">
-                                    <span className="vertical-divider"></span>
-                                </div>
-                                <div className="col-md-auto">
-                                    <span className="task-title">Higher than regular priority task</span>
-                                </div>
-                                <div className="col">
-                                    <div className="row no-gutters align-items-center d-flex justify-content-end">
-                                        <div className="col-md-auto">
-                                            <span className="task-due">Apr/01/19</span>
-                                        </div>
-                                        <div className="col-md-auto task-assignee-avatar">
-                                            <img className="avatar" src="/images/avatar.jpg" alt="Kate" />
-                                        </div>
-                                        <div className="col-md-auto">
-                                            <span className="task-assignee">Kate Wilson</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="color-coded-row code-grey task-row">
-                            <div className="row no-gutters align-items-center">
-                                <div className="col-md-auto task-status">
-                                    <span className="icon-info"></span>
-                                </div>
-                                <div className="col-md-auto">
-                                    <span className="icon-hash"></span>
-                                </div>
-                                <div className="col-md-auto">
-                                    <span className="task-key">ICC-215</span>
-                                </div>
-                                <div className="col-md-auto">
-                                    <span className="vertical-divider"></span>
-                                </div>
-                                <div className="col-md-auto">
-                                    <span className="task-title">Regular priority task</span>
-                                </div>
-                                <div className="col">
-                                    <div className="row no-gutters align-items-center d-flex justify-content-end">
-                                        <div className="col-md-auto">
-                                            <span className="task-due">Apr/01/19</span>
-                                        </div>
-                                        <div className="col-md-auto task-assignee-avatar">
-                                            <img className="avatar" src="/images/avatar.jpg" alt="Kate" />
-                                        </div>
-                                        <div className="col-md-auto">
-                                            <span className="task-assignee">Kate Wilson</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="color-coded-row code-grey task-row new-task-input">
-                            <div className="row no-gutters align-items-center">
-
-                                <div className="col-md-auto">
-                                    <span className="icon-hash"></span>
-                                </div>
-
-
-                                <div className="col">
-                                    {/*<span className="task-title">Regular priority task</span>*/}
-                                    <input type="text" name="new-task" autoFocus={true} placeholder="New task" />
-                                </div>
-                                <div className="col-md-auto">
-                                    <div className="row no-gutters align-items-center d-flex justify-content-end">
-                                        <div className="col-md-auto">
-                                            <span className="task-due">Apr/01/19</span>
-                                        </div>
-                                        <div className="col-md-auto task-assignee-avatar">
-                                            <img className="avatar" src="/images/avatar.jpg" alt="Kate" />
-                                        </div>
-                                        <div className="col-md-auto">
-                                            <span className="task-assignee">Kate Wilson</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-
-
-
-
-
-
+                        <TaskRow colorCode="red" taskType="info-circled" taskTitle={'Important task that requires special attention'} isEditing="true"/>
+                        <TaskRow colorCode="yellow" taskType="info" taskTitle={'Higher than regular priority task'} />
+                        <TaskRow colorCode="green" taskType="info-circled" taskTitle={'Regular priority task'} />
+                        <NewTaskRow colorCode="grey" taskType="info-attention"/>
 
                     </div>
-
-
-
                 </div>
-
-
-
-
-
             </CSSTransition>
         )
     }
 }
 
 export default TasksListing;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
